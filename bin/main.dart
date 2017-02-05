15cadabdf560336491f4371697f55c486943070f
reeFridge <reefridgerator@gmail.com>
2017-02-05 19:33:57 +0700

    Creating lockfile for detecting that daemon is already running

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,26 @@
 import 'dart:io';
+import 'dart:core';
 import 'package:args/args.dart';
 
 void main(List<String> args) {
-	stdout.writeln('Runs daemon and close.');
+	stdout.writeln('[DAEMON-PROOF] Runs daemon and close.');
+	File lockfile = new File('./daemon.lock');
+	lockfile.exists().then((bool exist) {
+		if (exist) {
+			int daemon_pid = int.parse(lockfile.readAsStringSync());
+			stdout.writeln('Daemon already runing.');
+			exit(0);
+		} else {
+			Process.start(
+				'dart',
+				['./bin/daemon.dart'],
+				mode: ProcessStartMode.DETACHED,
+				includeParentEnvironment: false
+			).then((Process proc) {
+				stdout.writeln('Daemon runs with pid: ${proc.pid}');
+				lockfile.writeAsStringSync(proc.pid.toString());
+				exit(0);
+			});
+		}
+	});
 }
