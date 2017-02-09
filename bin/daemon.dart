e8441ca2665cf4070910d66c0b257fdc22aeef3b
reeFridge <reefridgerator@gmail.com>
2017-02-09 21:47:24 +0700

    Listen SIGUSR1 for restart purpose

--- bin/daemon.dart
+++ bin/daemon.dart
@@ -2,19 +2,12 @@ import 'dart:io';
 import 'dart:core';
 
 void main(List<String> args) {
-	ProcessSignal.SIGUSR2.watch().listen((ProcessSignal sig) {
-		File file = new File('./log');
-		file.writeAsString('Daemon was received signal $sig');
-	});
-	
+	ProcessSignal.SIGUSR1.watch().listen((sig) => exit(0));
 	ProcessSignal.SIGTERM.watch().listen((ProcessSignal sig) {
 		File pidFile = new File('./pidfile');
 		pidFile.exists().then((bool exist) {
 			if (exist) {
-				int lock_pid = int.parse(pidFile.readAsStringSync());
-				if (lock_pid == pid) {
-					pidFile.deleteSync();
-				}
+				pidFile.deleteSync();
 			}
 			exit(0);
 		});
