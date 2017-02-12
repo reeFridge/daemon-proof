a2958d2e4a0e40866b0a256d0f59d34db7b30fb9
reeFridge <reefridgerator@gmail.com>
2017-02-13 00:39:02 +0700

    Forward dart script trough DaemonCommander arguments

--- bin/main.dart
+++ bin/main.dart
@@ -1,17 +1,25 @@
-import 'dart:core';
+import 'dart:io';
 import 'package:args/args.dart';
 import 'package:daemonproof/src/DaemonCommander.dart';
 
 void main(List<String> args) {
 	ArgParser parser = new ArgParser();
+	ArgResults results;
+	
 	parser.addOption('option',
 		abbr: 'o',
 		allowed: ['start', 'restart', 'kill'],
 		defaultsTo: 'start'
 	);
-	ArgResults results = parser.parse(args);
 	
-	DaemonCommander dc = new DaemonCommander();
+	try {
+		results = parser.parse(args);
+	} on FormatException catch(exception) {
+		print(exception.message);
+		exit(1);
+	}
+	
+	DaemonCommander dc = new DaemonCommander('./bin/daemon.dart');
 	
 	switch (results['option']) {
 		case 'kill':
